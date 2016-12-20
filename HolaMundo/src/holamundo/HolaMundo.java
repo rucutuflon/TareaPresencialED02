ba7d714f0f364cb7b54819d3064c0e3e7ba4101f
rucutuflon <rucutuflon@hotmail.com>
2016-12-20 18:53:17 +0100

    HolaHora

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -1,11 +1,20 @@
 package holamundo;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hola Mundo");
+		
+		Date date = new Date();
+		DateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
+		System.out.println("Hora: "+hourFormat.format(date));
+	
 	}
 
 }
